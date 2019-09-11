b9d298545f7d12e4f488c4fb88a6f7a9652c3cdb
Rost Myshkin <rost.myshkin@omnium.oi>
2019-09-11 11:57:50 -0700

    initial commit

---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Map extends Component {
+  render () {
+    return (
+      <div>Google map</div>
+    )
+  }
+}
+
+export default Map;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+  render () {
+    return (
+      <div>Nav Bar</div>
+    )
+  }
+}
+
+export default Nav;
