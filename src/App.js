25e1242b8faab80bcac8dc7254454e0a7bc2ba5f
Rost Myshkin <rost.myshkin@omnium.io>
2019-09-11 22:50:39 -0700

    added map integration with api key

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './App.css';
-import Map from './components/Map'
-import Nav from './components/Nav'
-import Search from './components/Search'
-import SideBar from './components/SideBar'
-
+import React from "react";
+import "./App.css";
+import Map from "./components/Map";
+import Nav from "./components/Nav/Nav";
+import Search from "./components/Search";
+import SideBar from "./components/SideBar";
+import { Container, Row, Col } from "react-bootstrap";
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <div>
         <Nav></Nav>
+
         <Map></Map>
         <Search></Search>
         <SideBar></SideBar>
       </div>
-    )
+    );
   }
 }
 export default App;
