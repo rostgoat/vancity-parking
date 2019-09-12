6015a06adf3d9c2ce509f858af26a7950378c15b
Rost Myshkin <rost.myshkin@omnium.io>
2019-09-11 23:47:59 -0700

    coordinates show up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
-import Map from "./components/Map";
+import Map from "./components/Map/Map";
 import Nav from "./components/Nav/Nav";
 import Search from "./components/Search";
 import SideBar from "./components/SideBar";
 import { Container, Row, Col } from "react-bootstrap";
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <Nav></Nav>
-
         <Map></Map>
         <Search></Search>
         <SideBar></SideBar>
