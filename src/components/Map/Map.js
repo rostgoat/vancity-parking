e4e189b611b58e4f27ab60696bab74b13a7140d2
Rost Myshkin <rost.myshkin@omnium.io>
2019-09-15 21:53:21 -0700

    marker icon

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,37 +4,41 @@ import * as meterData from "../../data/parking-meters1.json";
 import "./Map.css";
 
 function useMap() {
-  const [selectedMeter, setSelectedMeter] = useState(null)
-  
+  const [selectedMeter, setSelectedMeter] = useState(null);
+
   return (
-    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 49.2827, lng: -123.1207 }}> 
+    <GoogleMap defaultZoom={15} defaultCenter={{ lat: 49.2827, lng: -123.1207 }}>
       {meterData.data.map(meter => (
-        <Marker 
-          key={meter.recordid} 
-          position={{lat: meter.fields.geom.coordinates[1], lng: meter.fields.geom.coordinates[0]}}
+        <Marker
+          key={meter.recordid}
+          position={{ lat: meter.fields.geom.coordinates[1], lng: meter.fields.geom.coordinates[0] }}
           onClick={() => {
             setSelectedMeter(meter);
-          }}/>
-      )
-    )}
+          }}
+        />
+      ))}
 
-    {/* shows info window about meter. onCloseClick allows you to close the info window and open another one, reseting state */}
-    {selectedMeter && (
-      <InfoWindow
-        position={{lat: selectedMeter.fields.geom.coordinates[1], lng: selectedMeter.fields.geom.coordinates[0]}}
-        onCloseClick={() => {setSelectedMeter(null)}}
-        // icon={{
-        //   url: "../../../public/circle.svg",
-        //   scaledSize: new.target.google.maps.Size(25,25)
-        // }}
+      {/* shows info window about meter. onCloseClick allows you to close the info window and open another one, reseting state */}
+      {selectedMeter && (
+        <InfoWindow
+          position={{ lat: selectedMeter.fields.geom.coordinates[1], lng: selectedMeter.fields.geom.coordinates[0] }}
+          onCloseClick={() => {
+            setSelectedMeter(null);
+          }}
+          // icon={{
+          //   url: "../../../public/circle.svg",
+          //   scaledSize: new.target.google.maps.Size(25,25)
+          // }}
         >
-        <div>
-          <h3><b>Meter Information</b></h3>
-          <h4>PayByPhone : {selectedMeter.fields.pay_phone}</h4>
-          Rates: Mon-Fri 9:00 AM - 6:00 PM - {selectedMeter.fields.r_mf_9a_6p} ({selectedMeter.fields.t_mf_9a_6p})
-        </div>
-      </InfoWindow>
-    )} 
+          <div>
+            <h3>
+              <b>Meter Information</b>
+            </h3>
+            <h4>PayByPhone : {selectedMeter.fields.pay_phone}</h4>
+            Rates: Mon-Fri 9:00 AM - 6:00 PM - {selectedMeter.fields.r_mf_9a_6p} ({selectedMeter.fields.t_mf_9a_6p})
+          </div>
+        </InfoWindow>
+      )}
     </GoogleMap>
   );
 }
