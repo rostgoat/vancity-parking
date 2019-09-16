a42789895c9797639c5b84c61ca3459f84c0e9f0
Rost Myshkin <rost.myshkin@omnium.io>
2019-09-15 22:02:41 -0700

    added google api key to zshrc

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -49,7 +49,7 @@ class Map extends Component {
     return (
       <div className="map">
         <WrappedMap
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBlh-6hh0jO_I2c7FWR-vNzFsDqebeaL9`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `100vh` }} />}
           mapElement={<div style={{ height: `100%` }} />}
