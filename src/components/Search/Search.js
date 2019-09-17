6aa135d8948c371ff2f8f3735516eff767cf17f9
Rost Myshkin <rost.myshkin@omnium.io>
2019-09-17 12:01:28 -0700

    added new favicon; added search icon; more styling

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { Form, Button, Row, Col, FormControl, InputGroup } from "react-bootstrap";
 import "./Search.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 class Search extends Component {
   // method will search for specified street or address
-  handleSubmit = () => {};
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('clicked submit')
+  };
   render() {
     return (
       <div className="search">
         <Form onSubmit={this.handleSubmit}>
-          <Form.Group as={Row} controlId="searchForm">
-            <Col sm="10">
-              <Form.Control className="seach-form" type="text" placeholder="Enter street..." />
-            </Col>
-            <Col sm="2">
-              <Button variant="primary" type="submit" className="search-btn">
-                <FontAwesomeIcon icon={faSearch} className="seach-icon" />
-              </Button>
-            </Col>
+          <Form.Group controlId="searchForm" className="search-form">
+            <InputGroup>
+            <InputGroup.Prepend>
+                <FontAwesomeIcon icon={faSearch} className="search-icon" />
+            </InputGroup.Prepend>
+              <Form.Control className="search-input" type="text" placeholder="Enter street..." />
+            </InputGroup>
           </Form.Group>
         </Form>
       </div>
