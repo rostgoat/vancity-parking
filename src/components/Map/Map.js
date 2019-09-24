b2a284fde6960bb79f7097b1fce1312c50bf2590
Rost Myshkin <rost.myshkin@omnium.io>
2019-09-24 00:05:39 -0700

    clean up

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,62 +1,6 @@
-import React, { Component, useState } from "react";
-import * as meterData from "../../data/parking-meters1.json";
+import React, { Component } from "react";
 import "./Map.css";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import GoogleMapReact from "google-map-react";
-
-// const useMap = props => {
-//   const [selectedMeter, setSelectedMeter] = useState(null);
-//   let defaultLat, defaultLng;
-//   if (props.data) {
-//     defaultLat = props.data.data.records[0].fields.geom.coordinates[1];
-//     defaultLng = props.data.data.records[0].fields.geom.coordinates[0];
-//   } else {
-//     defaultLat = 49.2827;
-//     defaultLng = -123.1207;
-//   }
-
-//   return (
-//     <GoogleMap
-//       defaultZoom={14}
-//       defaultCenter={{ lat: defaultLat, lng: defaultLng }}
-//       panTo={{ lat: defaultLat, lng: defaultLng }}
-//     >
-//       {props.data
-//         ? props.data.data.records.map(meter => (
-//             <Marker
-//               key={meter.recordid}
-//               position={{ lat: meter.fields.geom.coordinates[1], lng: meter.fields.geom.coordinates[0] }}
-//               onClick={() => {
-//                 setSelectedMeter(meter);
-//               }}
-//             />
-//           ))
-//         : null}
-
-//       {/* shows info window about meter. onCloseClick allows you to close the info window and open another one, reseting state */}
-//       {selectedMeter && (
-//         <InfoWindow
-//           position={{ lat: selectedMeter.fields.geom.coordinates[1], lng: selectedMeter.fields.geom.coordinates[0] }}
-//           onCloseClick={() => {
-//             setSelectedMeter(null);
-//           }}
-//           // icon={{
-//           //   url: "../../../public/circle.svg",
-//           //   scaledSize: new.target.google.maps.Size(25,25)
-//           // }}
-//         >
-//           <div>
-//             <h3>
-//               <b>Meter Information</b>
-//             </h3>
-//             <h4>PayByPhone : {selectedMeter.fields.pay_phone}</h4>
-//             Rates: Mon-Fri 9:00 AM - 6:00 PM - {selectedMeter.fields.r_mf_9a_6p} ({selectedMeter.fields.t_mf_9a_6p})
-//           </div>
-//         </InfoWindow>
-//       )}
-//     </GoogleMap>
-//   );
-// };
 
 class Map extends Component {
   static defaultProps = {
