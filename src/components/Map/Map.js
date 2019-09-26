f1958f89a25e445554cb2a62b43662cef976f768
Rost Myshkin <rost.myshkin@omnium.io>
2019-09-25 23:51:43 -0700

    map recenters when new props are passed to map class

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,26 +3,46 @@ import "./Map.css";
 import GoogleMapReact from "google-map-react";
 import Marker from "../Marker/Marker";
 
+// constant initial values
 const defaultLat = 49.2827;
 const defaultLng = -123.1207;
 const defaultZoom = 14;
 
+/**
+ * Map Class that renders the Google Map
+ */
 class Map extends Component {
   state = {
     center: [defaultLat, defaultLng],
     zoom: defaultZoom
   };
 
-  onChange = (center, zoom) => {
-    this.setState({
-      center: center,
-      zoom: zoom
-    });
-  };
+  /**
+   * Method re-centers map whenever new props are passed in
+   *
+   * @param { Object } props
+   * @param { Object } state
+   */
+  static getDerivedStateFromProps(props, state) {
+    const oldCenter = state.center;
+
+    if (props.searchedResponse !== null) {
+      const newCenter = props.searchedResponse.data.records[0].fields.geom.coordinates;
+
+      const [newLng, newLat] = [...newCenter];
+      const [oldLat, oldLng] = [...oldCenter];
+
+      if (oldLat !== newLat && oldLng !== newLng) {
+        return {
+          center: [newLat, newLng]
+        };
+      }
+    }
+    return null;
+  }
 
   render() {
     const data = this.props.searchedResponse;
-    console.log("data", data);
     const Markers = data
       ? data.data.records.map(marker => (
           <Marker
@@ -37,10 +57,10 @@ class Map extends Component {
     return (
       <div className="map">
         <GoogleMapReact
-          defaultCenter={this.state.center}
+          center={this.state.center}
           defaultZoom={this.state.zoom}
           bootstrapURLKeys={{
-            key: "AIzaSyBlh-6hh0jO_I2c7FWR-vNzFsDqebeaL9I"
+            key: process.env.REACT_APP_GOOGLE_KEY
           }}
         >
           {Markers}
