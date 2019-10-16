99502ca94fd3d7a318dd973e7df4bf91a4771091
Rost Myshkin <rost.myshkin@omnium.io>
2019-10-15 23:30:44 -0700

    marker styling

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./Map.scss";
 // import Marker from "../Marker/Marker";
 import { GoogleMap, LoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import "../Marker/Marker.scss";
 // constant initial values
 const lat = 49.2827;
 const lng = -123.1207;
@@ -43,13 +44,19 @@ class MapContainer extends Component {
     return null;
   }
 
-  onMarkerClick = marker => {
+  onMarkerClick = (e, marker) => {
     console.log("marker", marker);
     this.setState({
       selectedMarker: marker.recordid,
       showingInfoWindow: true
     });
   };
+
+  onSendMarkerInfoToParent = e => {
+    console.log("g");
+    this.props.onSendMarkerInfoToParent(e);
+  };
+
   render() {
     const data = this.props.searchedResponse;
     const Markers = props =>
@@ -59,11 +66,21 @@ class MapContainer extends Component {
               <Marker
                 position={{ lat: marker.fields.geom.coordinates[1], lng: marker.fields.geom.coordinates[0] }}
                 key={marker.recordid}
-                onClick={() => this.onMarkerClick(marker)}
+                onClick={e => this.onMarkerClick(e, marker)}
+                icon={{
+                  path:
+                    "M12.75 0l-2.25 2.25 2.25 2.25-5.25 6h-5.25l4.125 4.125-6.375 8.452v0.923h0.923l8.452-6.375 4.125 4.125v-5.25l6-5.25 2.25 2.25 2.25-2.25-11.25-11.25zM10.5 12.75l-1.5-1.5 5.25-5.25 1.5 1.5-5.25 5.25z",
+                  fillColor: "#0000ff",
+                  fillOpacity: 1.0,
+                  strokeWeight: 0,
+                  scale: 1.25
+                }}
               >
                 {this.state.showingInfoWindow && this.state.selectedMarker === marker.recordid && (
                   <InfoWindow
+                    className="info-window"
                     position={{ lat: marker.fields.geom.coordinates[1], lng: marker.fields.geom.coordinates[0] }}
+                    onSendMarkerInfoToParent={this.onSendMarkerInfoToParent}
                   >
                     <div>{marker.recordid}</div>
                   </InfoWindow>
@@ -74,7 +91,7 @@ class MapContainer extends Component {
         : null;
     return (
       <div className="map-container">
-        <LoadScript googleMapsApiKey="AIzaSyBlh-6hh0jO_I2c7FWR-vNzFsDqebeaL9I">
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_KEY}>
           <GoogleMap id="map" center={this.state.center} zoom={this.state.zoom}>
             <Markers />
           </GoogleMap>
