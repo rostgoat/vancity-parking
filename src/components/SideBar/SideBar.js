31ef45d8efb800f52d2d52a3563c38c9ffd96563
Rost Myshkin <rost.myshkin@omnium.io>
2019-11-21 11:57:47 -0800

    a million changes

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -2,72 +2,54 @@ import React, { Component } from "react";
 import "./SideBar.scss";
 import { FaCcVisa, FaCoins } from "react-icons/fa";
 import { connect } from "react-redux";
+import { rateTimeCalc } from "../../utils/utils";
 
 class SideBar extends Component {
   render() {
     const { areas } = this.props.areas;
-
+    console.log(
+      "areas && areas.data && areas.data.records",
+      areas && areas.data && areas.data.records ? rateTimeCalc(areas.data.records[0]) : null
+    );
     const sidebarItems =
       areas && areas.data && areas.data.records
-        ? areas.data.records.map(item => (
-            <div className="sidebar-item" key={item.recordid}>
-              <div className="sidebar-item__info">
-                <div className="sidebar-item__info-basic">
-                  <div className="sidebar-item__info-basic--meter">{item.fields.pay_phone}</div>
-                  <div className="sidebar-item__info-basic--street">2800 Hastings</div>
-                  <div className="sidebar-item__info-basic--payment">
-                    {item.fields.creditcard ? (
+        ? areas.data.records.map(item => {
+            console.log("item", item);
+            item = rateTimeCalc(item);
+            return (
+              <div className="sidebar-item" key={item.recordid}>
+                <div className="sidebar-item__info">
+                  <div className="sidebar-item__info-basic">
+                    <div className="sidebar-item__info-basic--left sidebar-item__info-basic--meter">
+                      {item.pay_phone}
+                    </div>
+                  </div>
+                  <div className="sidebar-item__info-time-rates">
+                    <div className="sidebar-item__info-time-rates--row">{item.day}</div>
+                    <div className="sidebar-item__info-time-rates--row">
+                      <div className="sidebar-item__info-time-rates--col">9:00am - 6:00pm</div>
+                    </div>
+                    <div className="sidebar-item__info-time-rates--row">
+                      <div className="sidebar-item__info-time-rates--col">{item.rate_per_hour}</div>/
+                      <div className="sidebar-item__info-time-rates--col">{item.length_of_stay}</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="sidebar-item__footer">
+                  <div className="sidebar-item__footer--item">
+                    {item.creditcard ? (
                       <div>
-                        <FaCcVisa size="2em" /> / <FaCoins size="1.5em" />
+                        <FaCcVisa size="2em" className="sidebar-item__footer--item-payment" />
+                        <FaCoins size="1.5em" className="sidebar-item__footer--item-payment" />
                       </div>
                     ) : (
-                      <FaCoins size="2em" />
+                      <FaCoins size="2em" className="sidebar-item__footer--item-payment" />
                     )}
                   </div>
                 </div>
-                <div className="sidebar-item__info-time-rates">
-                  <div className="sidebar-item__info-time-rates--row">
-                    <div className="sidebar-item__info-time-rates--col">Mon - Fri</div>
-                    <div className="sidebar-item__info-time-rates--col">9:00am - 6:00pm</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.t_mf_9a_6p}</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.r_mf_9a_6p}</div>
-                  </div>
-                  <div className="sidebar-item__info-time-rates--row">
-                    <div className="sidebar-item__info-time-rates--col">Mon - Fri</div>
-                    <div className="sidebar-item__info-time-rates--col">6:00am - 10:00pm</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.t_mf_6p_10}</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.r_mf_6p_10}</div>
-                  </div>
-
-                  <div className="sidebar-item__info-time-rates--row">
-                    <div className="sidebar-item__info-time-rates--col">Sat</div>
-                    <div className="sidebar-item__info-time-rates--col">9:00am - 6:00pm</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.t_sa_9a_6p}</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.r_sa_9a_6p}</div>
-                  </div>
-                  <div className="sidebar-item__info-time-rates--row">
-                    <div className="sidebar-item__info-time-rates--col">Sat</div>
-                    <div className="sidebar-item__info-time-rates--col">6:00am - 10:00pm</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.t_sa_6p_10}</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.r_sa_6p_10}</div>
-                  </div>
-
-                  <div className="sidebar-item__info-time-rates--row">
-                    <div className="sidebar-item__info-time-rates--col">Sun</div>
-                    <div className="sidebar-item__info-time-rates--col">9:00am - 6:00pm</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.t_su_9a_6p}</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.r_su_9a_6p}</div>
-                  </div>
-                  <div className="sidebar-item__info-time-rates--row">
-                    <div className="sidebar-item__info-time-rates--col">Sun</div>
-                    <div className="sidebar-item__info-time-rates--col">6:00am - 10:00pm</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.t_su_6p_10}</div>
-                    <div className="sidebar-item__info-time-rates--col">{item.fields.r_su_6p_10}</div>
-                  </div>
-                </div>
               </div>
-            </div>
-          ))
+            );
+          })
         : null;
     return (
       <div className="sidebar">
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,15 @@
+const formatAMPM = date => {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+};
 /**
- * Function returns rate of marker based on current day and time
+ * Function returns length_of_stay of marker based on current day and time
  *
  * @param {Object} marker - marker object from the map
  */
@@ -9,48 +19,113 @@ export const rateTimeCalc = marker => {
   const today = new Date();
   const day = today.getDay();
   const hours = today.getHours();
+  const currentTime = formatAMPM(today);
+
+  const days = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday"
+  };
 
   // weekdays
   if (day >= 1 || day <= 5) {
     // 9 am - 6pm
     if (hours >= 9 && hours < 18) {
-      return fields.r_mf_9a_6p;
+      return {
+        rate_per_hour: fields.r_mf_9a_6p,
+        length_of_stay: fields.t_mf_9a_6p,
+        day: days[day],
+        currentTime,
+        creditcard: fields.creditcard,
+        pay_phone: fields.pay_phone
+      };
 
       // 6pm - 10pm
     } else if (hours >= 18 && hours < 22) {
-      return fields.r_mf_6p_10;
+      return {
+        rate_per_hour: fields.r_mf_6p_10,
+        length_of_stay: fields.t_mf_6p_10,
+        day: days[day],
+        currentTime,
+        creditcard: fields.creditcard,
+        pay_phone: fields.pay_phone
+      };
 
       // 10pm - 9am
     } else {
-      return "Free";
+      return {
+        length_of_stay: "Free",
+        day: days[day],
+        currentTime
+      };
     }
     // Saturday
   } else if (day === 6) {
     // 9 am - 6pm
     if (hours >= 9 && hours < 18) {
-      return fields.r_sa_9a_6p;
+      return {
+        rate_per_hour: fields.r_sa_9a_6p,
+        length_of_stay: fields.t_sa_9a_6p,
+        day: days[day],
+        currentTime,
+        creditcard: fields.creditcard,
+        pay_phone: fields.pay_phone
+      };
 
       // 6pm - 10pm
     } else if (hours >= 18 && hours < 22) {
-      return fields.r_sa_6p_10;
+      return {
+        rate_per_hour: fields.r_sa_6p_10,
+        length_of_stay: fields.t_sa_6p_10,
+        day: days[day],
+        currentTime,
+        creditcard: fields.creditcard,
+        pay_phone: fields.pay_phone
+      };
 
       // 10pm - 9am
     } else {
-      return "Free";
+      return {
+        length_of_stay: "Free",
+        day: days[day],
+        currentTime
+      };
     }
     // Sunday
   } else if (day === 0) {
     // 9 am - 6pm
     if (hours >= 9 && hours < 18) {
-      return fields.r_su_9a_6p;
+      return {
+        rate_per_hour: fields.r_su_9a_6p,
+        length_of_stay: fields.t_su_9a_6p,
+        day: days[day],
+        currentTime,
+        creditcard: fields.creditcard,
+        pay_phone: fields.pay_phone
+      };
 
       // 6pm - 10pm
     } else if (hours >= 18 && hours < 22) {
-      return fields.r_su_6p_10;
+      return {
+        rate_per_hour: fields.r_su_6p_10,
+        length_of_stay: fields.t_su_6p_10,
+        day: days[day],
+        currentTime,
+        creditcard: fields.creditcard,
+        pay_phone: fields.pay_phone
+      };
 
       // 10pm - 9am
     } else {
-      return "Free";
+      return {
+        length_of_stay: "Free",
+        day: days[day],
+        currentTime
+      };
     }
   }
 };
